ba55b27a328f9126a5cfc6996b12f29213c43f4b
Nityananda Gohain <nityanandagohain@gmail.com>
2019-11-18 10:17:29 +0530

    artist details fetched

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,6 @@
+require "net/http"
+require "json"
+
 class ArtistsController < ApplicationController
     before_action :authorize, only: [:create, :index, :new]
 
@@ -5,6 +8,8 @@ class ArtistsController < ApplicationController
         @artist = Artist.new
         user = User.find_by_id(current_user.id)
         @previous_artists =  user.artists
+
+        # get_artist_details()
     end
 
     def create
@@ -15,6 +20,31 @@ class ArtistsController < ApplicationController
         else            
             puts "already present in database"
         end
+
+        @artist_details = get_artist_details(@artist.name)
+
+        render "result"
+    end
+
+    def get_artist_details(name)
+        # get artist info
+        result = {}
+        url = URI.parse("http://ws.audioscrobbler.com/2.0/?format=json&api_key=#{ENV["LASTFM_KEY"]}&method=artist.getInfo&artist=#{name}")
+        res = Net::HTTP.start(url.host, url.port) { |http|
+            http.request(Net::HTTP::Get.new(url.to_s))
+        }
+        data = JSON.parse(res.body)
+        result["artist"] = data["artist"]
+
+        # get top tracks of the artist
+        url = URI.parse("http://ws.audioscrobbler.com/2.0/?format=json&api_key=#{ENV["LASTFM_KEY"]}&method=artist.gettoptracks&artist=#{name}&limit=10")
+        res = Net::HTTP.start(url.host, url.port) { |http|
+            http.request(Net::HTTP::Get.new(url.to_s))
+        }
+        data = JSON.parse(res.body)
+        result["toptracks"] = data["toptracks"]
+        # puts result["toptracks"]
+        return result
     end
 
     
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,3 @@
 class Artist < ApplicationRecord
     belongs_to :user
-
-    validates :name, presence: true, uniqueness: true
 end
