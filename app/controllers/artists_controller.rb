f3dc103178ef82ef83b3cb6cf2834741d52ff9dc
Nityananda Gohain <nityanandagohain@gmail.com>
2019-11-21 22:53:23 +0530

    comments

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -15,12 +15,14 @@ class ArtistsController < ApplicationController
     def create
         @artist = Artist.new(post_params)
         user = User.find_by_id(current_user.id)
+        # If the user already searched for the artist then it is not added else it is added
         if not user.artists.exists? name: @artist.name
             user.artists << @artist
         else            
             puts "already present in database"
         end
 
+        # Fastfm api is called for the artist details and toptracks
         @artist_details = {}
         @artist_details["artist"] = helpers.getArtistInfo(Uri, @artist.name)
         @artist_details["toptracks"] = helpers.getTopTracks(Uri, @artist.name)
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,7 @@ require "json"
 
 module ArtistsHelper
     def getArtistInfo(uri, name)
+        # query params for the api request
         params = { :format => 'json', :api_key => ENV["LASTFM_KEY"],:method => 'artist.getinfo', :artist => name }
         uri.query = URI.encode_www_form(params)
         begin
@@ -19,6 +20,7 @@ module ArtistsHelper
     end
 
     def getTopTracks(uri, name)
+        # query params for the api request
         params = { :format => 'json', :api_key => ENV["LASTFM_KEY"],:method => 'artist.gettoptracks', :artist => name, :limit => 10 }
         uri.query = URI.encode_www_form(params)
         begin
