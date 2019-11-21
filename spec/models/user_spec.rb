3db4be53f28214efc7672709891d18782925c3b9
Nityananda Gohain <nityanandagohain@gmail.com>
2019-11-21 20:04:15 +0530

    more test added for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe User, type: :model do
       expect(user).to eq(false)
     end
 
+    it 'ensures the vaild email' do 
+      user = User.new(email: '12xyz1@', password: 'heyypassword').save
+      expect(user).to eq(false)
+      user = User.new(email: 'xyz1@##', password: 'heyypassword').save
+      expect(user).to eq(false)
+      user = User.new(email: 'xyz1@xyz', password: 'heyypassword').save
+      expect(user).to eq(true)
+    end
+
+    it 'ensures the password length is greater than five' do 
+      user = User.new(email: 'xyz1@xyz.com', password: 'pass').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures the users are not created with same email' do 
+      user1 = User.new(email: 'xyz1@xyz.com', password: 'heyypassword1').save
+      user2 = User.new(email: 'xyz1@xyz.com', password: 'heyypassword2').save
+      expect(user1).to eq(true)
+      expect(user2).to eq(false)
+    end
+
     it 'should save successfully' do
       user = User.new(email: 'xyz1@xyz.com', password: 'heyypassword').save
       expect(user).to eq(true)
