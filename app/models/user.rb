5ac34128a5d044cacc52b602cf053f7957fb9132
Nityananda Gohain <nityanandagohain@gmail.com>
2019-11-21 19:18:07 +0530

    email validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, presence: true, length: {minimum: 5}
 
   has_many :artists
